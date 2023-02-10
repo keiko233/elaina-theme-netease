537977a07243932907acc874bab9a40cb9f2c8eb
keiko233 <keiko23333@gmail.com>
2023-02-11 04:04:09 +0800

    feat: add style for cover card

--- main.js
+++ main.js
@@ -220,6 +220,15 @@ plugin.onLoad(async () => {
     border-color: #282828 transparent transparent #282828 !important;
     background-color: #282828 !important;
   }
+
+  .cvr.u-cover.u-cover-recmd {
+    border-radius: 24px;
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+  }
+
+  .u-cover .ci {
+    padding-right: 16px;
+  }
   `;
   const style = document.createElement("style");
   style.innerHTML = css;
