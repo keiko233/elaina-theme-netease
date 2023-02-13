2b4aa2042bc704b99e048d42dc81b86e27b1d734
keiko233 <keiko23333@gmail.com>
2023-02-13 19:17:44 +0800

    refactor: by ChatGPT

--- build.js
+++ build.js
@@ -2,14 +2,17 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const fs = require('fs');
 
-if (process.argv[2] == "delete") {
-  fs.unlink("./public/manifest.json", function(err) {
-    if (err) console.error("\x1B[31mDelete manifest.json err\x1B[0m");
-    else console.log("\x1B[32mDelete success manifest.json on public\x1B[0m");
-  })
-} else {
-  fs.copyFile("./manifest.json", "./public/manifest.json", function (err) {
-    if (err) console.error("\x1B[31mCopy manifest.json err\x1B[0m");
-    else console.log("\x1B[32mCopy success manifest.json to public\x1B[0m");
-  })
+switch (process.argv[2]) {
+  case "delete":
+    fs.unlink("./public/manifest.json", function (err) {
+      if (err) console.error("\x1B[31mDelete manifest.json err\x1B[0m");
+      else console.log("\x1B[32mDelete success manifest.json on public\x1B[0m");
+    });
+    break;
+  default:
+    fs.copyFile("./manifest.json", "./public/manifest.json", function (err) {
+      if (err) console.error("\x1B[31mCopy manifest.json err\x1B[0m");
+      else console.log("\x1B[32mCopy success manifest.json to public\x1B[0m");
+    });
+    break;
 }
