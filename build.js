6d0325a0faed29fff7c3e1dca5a5adaf0c506327
keiko233 <keiko23333@gmail.com>
2023-02-15 02:38:31 +0800

    feat: rewrite build.js

--- build.js
+++ build.js
@@ -2,17 +2,40 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const fs = require('fs');
 
+function copyFile(filePath, copyPath) {
+  fs.copyFile("./" + filePath, "./" + copyPath + "/" + filePath, function (err) {
+    if (err) console.error("\x1B[31mCopy " + filePath + " file err\x1B[0m\n" + err);
+    else console.log("\x1B[32mCopy " + filePath + " file success\x1B[0m");
+  });
+}
+
+function deleteFile(filePath) {
+  fs.unlink("./" + filePath, function (err) {
+    if (err) console.error("\x1B[31mDelete " + filePath + " file err\x1B[0m\n" + err);
+    else console.log("\x1B[32mDelete " + filePath + " file success\x1B[0m");
+  });
+}
+
+const fileList = [
+  {
+    from: "manifest.json",
+    to: "public"
+  },
+  {
+    from: "preview.png",
+    to: "public"
+  }
+]
+
 switch (process.argv[2]) {
   case "delete":
-    fs.unlink("./public/manifest.json", function (err) {
-      if (err) console.error("\x1B[31mDelete manifest.json err\x1B[0m");
-      else console.log("\x1B[32mDelete success manifest.json on public\x1B[0m");
-    });
+    for (var i = 0; i < fileList.length; i++) {
+      deleteFile(fileList[i].to + "/" + fileList[i].from);
+    }
     break;
   default:
-    fs.copyFile("./manifest.json", "./public/manifest.json", function (err) {
-      if (err) console.error("\x1B[31mCopy manifest.json err\x1B[0m");
-      else console.log("\x1B[32mCopy success manifest.json to public\x1B[0m");
-    });
+    for (var i = 0; i < fileList.length; i++) {
+      copyFile(fileList[i].from, fileList[i].to);
+    }
     break;
 }
