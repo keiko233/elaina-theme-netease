a8681b64c8ae3f9c7f33bd0345db97b42ba64796
keiko233 <keiko23333@gmail.com>
2023-02-22 18:11:32 +0800

    feat: add app template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <ThemeConfig />
+  <div class="container">
+    <div class="loadcard">
+      <ThemeConfig />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,6 +11,23 @@ import ThemeConfig from './components/vue/ThemeConfig.vue'
 
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
 
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .loadcard {
+    background-color: var(--background-overlay);
+    padding: 24px 36px;
+    border-radius: var(--border-raduis);
+  }
+}
 </style>
