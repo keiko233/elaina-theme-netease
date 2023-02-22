5dd24d2c54113f84b6c7017304ac9d0eed5923a4
keiko233 <keiko23333@gmail.com>
2023-02-22 22:52:56 +0800

    feat: add playerStyle config

--- src/components/vue/ThemeConfig.vue
+++ src/components/vue/ThemeConfig.vue
@@ -3,6 +3,19 @@
     <div class="title">Elaina Theme Config</div>
 
     <div class="config-card">
+      <h3>使用主题内置的播放界面</h3>
+      <p>默认开启。如果遇到问题可以尝试关闭，主题修改了部分CSS具有入侵性，可能会导致和而其他插件不兼容导致布局出错。</p>
+      <n-switch v-model:value="playerStyle" @update:value="playerStyleSwitch" size="large">
+        <template #checked>
+          启用主题播放界面
+        </template>
+        <template #unchecked>
+          关闭主题播放界面
+        </template>
+      </n-switch>
+    </div>
+
+    <div class="config-card">
       <h3>播放界面歌曲名显示</h3>
       <p>开启效果很不错，默认不开启，防止与其他插件发生不兼容情况。</p>
       <n-switch v-model:value="songNameStatus" @update:value="songNameSwitch" size="large">
@@ -59,10 +72,35 @@ const songNameSwitch = (value) => {
   songNameOnTop(value);
 }
 
+const setThemePlayStyle = (value) => {
+  const header = document.getElementById('music-163-com');
+
+  if (value) {
+    header.classList.add('theme-player');
+  } else {
+    header.classList.remove('theme-player');
+  }
+}
+
+if (!localStorage.getItem('enableThemePlayStyle')) {
+  localStorage.setItem('enableThemePlayStyle', 'true')
+  setThemePlayStyle(true);
+}
+
+const playerStyle = ref(JSON.parse(localStorage.getItem('enableThemePlayStyle')));
+
+const playerStyleSwitch = (value) => {
+  localStorage.setItem('enableThemePlayStyle', value);
+  setThemePlayStyle(value);
+}
+
 onMounted(() => {
   console.log("onMounted");
   if (songNameStatus.value == true) {
     songNameOnTop(true);
+  }
+  if (playerStyle.value == true) {
+    setThemePlayStyle(true);
   }
 })
 </script>
