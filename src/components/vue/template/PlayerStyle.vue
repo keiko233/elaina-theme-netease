3a86924a95499dd3b08074b8937714108388ebf5
keiko233 <keiko23333@gmail.com>
2023-02-23 08:02:00 +0800

    fix: modify string

--- src/components/vue/template/PlayerStyle.vue
+++ src/components/vue/template/PlayerStyle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="config-card">
     <h3>使用主题内置的播放界面</h3>
-    <p>默认开启。如果遇到问题可以尝试关闭，主题修改了部分CSS具有入侵性，可能会导致和而其他插件不兼容导致布局出错。</p>
+    <p>默认开启，本功能修改了部分CSS具有入侵性，可能会导致和而其他插件不兼容导致布局出错。如果遇到问题可以尝试关闭，关闭后将使用原本的播放界面</p>
     <n-switch v-model:value="playerStyleStatus" @update:value="playerStyleSwitch" size="large">
       <template #checked>
         启用主题播放界面
