9a01c9d27d5d274157fc5560e38c2aecdbf72255
keiko233 <keiko23333@gmail.com>
2023-02-27 16:27:03 +0800

    feat: request bocchiLists from gist

--- src/components/vue/template/BocchiOverlay.vue
+++ src/components/vue/template/BocchiOverlay.vue
@@ -2,13 +2,16 @@
   <div class="config-card">
     <n-spin :show="bocchiOverlayLoadingStatus">
       <h3>波奇酱小挂件</h3>
-      <p>启用需要网络连接，用于下载图片。给朋友做的彩蛋功能，没啥用，纯粹好玩。</p>
+      <p>启用需要网络连接，用于请求Gist和下载图片。给朋友做的彩蛋功能，没啥用，纯粹好玩。</p>
       <n-radio-group v-model:value="bocchiOverlaySelect" @update:value="bocchiOverlaySwitch">
         <n-radio-button value=-1 label='禁用' />
         <n-radio-button v-for="bocchiList in bocchiLists" :key="bocchiList.id" :value="bocchiList.id"
-          :label="bocchiList.name" />
+          :label="bocchiList.name" :disabled="bocchiList.id == null" />
         <n-radio-button value=-2 label='自定义' />
       </n-radio-group>
+      <template #description>
+        请求Gist中
+      </template>
     </n-spin>
 
     <div class="content">
@@ -58,29 +61,23 @@ if (!localStorage.getItem('bocchiOverlaySelectValue')) {
 const bocchiOverlaySelect = ref(JSON.parse(localStorage.getItem('bocchiOverlaySelectValue')));
 const bocchiOverlayLoadingStatus = ref(false);
 
-const bocchiLists = reactive([
+const bocchiLists = ref([
   {
-    id: 0,
-    name: 'Bocchi',
-    url: 'https://gist.githubusercontent.com/keiko233/a61d922c32a9b05b3ff2623305dd3faf/raw/3a0892747b5feabeda841687416ee5a5cb437beb/base64image_bocchi'
-  },
-  {
-    id: 1,
-    name: 'Elaina',
-    url: 'https://gist.githubusercontent.com/keiko233/a61d922c32a9b05b3ff2623305dd3faf/raw/78c89c3fd28d99966d1f91fabe3f8b050f223a73/base64image_elaina'
+    id: null,
+    name: 'Loading',
+    url: ''
   }
 ])
 
 const bocchiOverlaySwitch = (value) => {
   localStorage.setItem('bocchiOverlaySelectValue', value);
-  console.log(value);
   if (value == -1) {
     setBocchiOverlay(false);
   } else if (value == -2) {
 
   } else {
     bocchiOverlayLoadingStatus.value = true;
-    fetch(bocchiLists[value].url)
+    fetch(bocchiLists.value[value].url)
       .then((response) => response.json())
       .then((json) => {
         localStorage.setItem('bocchiOverlayImageData', json.response[0].content);
@@ -104,10 +101,21 @@ const updateBocchiImage = (id) => {
   }
 }
 
+const getBocchiLists = () => {
+  bocchiOverlayLoadingStatus.value = true;
+  fetch('https://gist.githubusercontent.com/keiko233/a61d922c32a9b05b3ff2623305dd3faf/raw/list')
+    .then((response) => response.json())
+    .then((json) => {
+      bocchiOverlayLoadingStatus.value = false;
+      bocchiLists.value = json.response;
+    });
+}
+
 onMounted(() => {
   if (bocchiOverlaySelect.value >= 0 && isNCMClient()) {
     setBocchiOverlay(true);
   }
+  getBocchiLists();
 })
 </script>
 
