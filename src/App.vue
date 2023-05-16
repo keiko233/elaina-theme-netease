f2f27a88fbe5449e64713a3be22d91323d282f0b
keiko233 <keiko23333@gmail.com>
2023-05-16 21:19:22 +0800

    Initial Framework with Vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-space>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </a-space>
+
+  <a-card title="Default size card" style="width: 300px">
+    <template #extra><a href="#">more</a></template>
+    <p>card content</p>
+    <p>card content</p>
+    <p>card content</p>
+  </a-card>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    })
+  ],
+  build: {
+    assetsDir: "./",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+        format: 'umd'
+      }
+    }
+  }
+})
