4fbd7cba56fcf7839af8f51db4426fc6b4cf8299
keiko233 <keiko23333@gmail.com>
2023-05-22 17:04:54 +0800

    feat: add Header Easter egg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,56 @@
 <template>
-  <ConfigCard class="config-card">
-    <div class="container">
+  <ConfigCard class="config-card" @click="click">
+    <div class="container" :class="vClass">
       <n-gradient-text :size="36" :gradient="title" class="title">
-        Elaina Theme
+        {{ context[index] }}
       </n-gradient-text>
     </div>
   </ConfigCard>
 </template>
 
 <script setup lang="ts">
+import packageJson from '../../package.json';
+
 const title = ref({
   deg: 45,
   from: '#FF487A',
   to: '#FF8FBA'
 });
+
+const context = ref([
+  "Elaina Theme",
+  "Version: " + packageJson.version,
+  "Author: " + packageJson.author,
+  "这是个小彩蛋哦"
+]);
+
+const index = ref(0);
+const vClass = ref('');
+
+function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+let lock = false;
+const click = async () => {
+  if (lock == false) {
+    for (let i = 1; i < context.value.length; i++) {
+      lock = true;
+      vClass.value = 'fade-out';
+      await wait(1000);
+      index.value++;
+      vClass.value = 'fade-in';
+      await wait(2000);
+      if (i == context.value.length - 1) {
+        vClass.value = 'fade-out';
+        await wait(1000);
+        index.value = 0;
+        vClass.value = 'fade-in';
+        lock = false;
+      }
+    };
+  }
+};
 </script>
 
 <style scoped lang="less">
@@ -65,6 +102,42 @@ const title = ref({
       75% {
         transform: scale(0.975) rotate(-0.005turn);
       }
+    }
+  }
+}
+
+.fade-out {
+  animation: fadeoutAnimation 1s normal 1 forwards;
+
+  @keyframes fadeoutAnimation {
+    0% {
+      transform: scale(1) rotate(0);
+      opacity: 1;
+      filter: blur(0);
+    }
+
+    100% {
+      transform: scale(0.2) rotate(0.01turn);
+      opacity: 0;
+      filter: blur(12px);
+    }
+  }
+}
+
+.fade-in {
+  animation: fadeinAnimation 1s normal 1 forwards;
+
+  @keyframes fadeinAnimation {
+    0% {
+      transform: scale(0.2) rotate(-0.01turn);
+      opacity: 0;
+      filter: blur(12px);
+    }
+
+    100% {
+      transform: scale(1) rotate(0);
+      opacity: 1;
+      filter: blur(0);
     }
   }
 }
