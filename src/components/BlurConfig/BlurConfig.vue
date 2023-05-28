2fd01905f5e0635689e36d180da14ac0b849c7de
keiko233 <keiko23333@gmail.com>
2023-05-28 19:52:02 +0800

    style: migrate BlurConfig to folder
    * Migrate & export functions to .ts

--- src/components/BlurConfig/BlurConfig.vue
+++ src/components/BlurConfig/BlurConfig.vue
@@ -34,68 +34,16 @@
 </template>
 
 <script setup lang="ts">
-import { initLS, putLS } from '../utils/localStorage';
-import { insertStyle, removeStyle } from '../utils/styleInsert';
-import ConfigCard from './ConfigCard.vue';
+import { 
+  insertCustomBlurStyle,
+  updateCustomBlur,
+  elementLists,
+  removeCustomBlurStyle
+} from '.';
+import { initLS, putLS } from '../../utils/localStorage';
+import ConfigCard from '../ConfigCard.vue';
 
 const customBlurStatus = ref(initLS('elaina-customBlurStatus', false));
-
-const elementLists = ref([
-  { 
-    name: '主页背景', 
-    value: initLS('elaina-blurHomeValue', 0), 
-    lsKey: 'elaina-blurHomeValue', 
-    style: '--theme-blur-home' 
-  },
-  { 
-    name: '主页边框', 
-    value: initLS('elaina-blurHomeFrameValue', 4), 
-    lsKey: 'elaina-blurHomeFrameValue', 
-    style: '--theme-blur-home-frame' 
-  },
-  { 
-    name: '播放页', 
-    value: initLS('elaina-blurPlayValue', 8), 
-    lsKey: 'elaina-blurPlayValue', 
-    style: '--theme-blur-play' 
-  },
-  { 
-    name: '播放页底栏', 
-    value: initLS('elaina-blurPlayDockValue', 32), 
-    lsKey: 'elaina-blurPlayDockValue', 
-    style: '--theme-blur-play-dock' 
-  },
-  { 
-    name: '设置卡片', 
-    value: initLS('elaina-blurConfigCardValue', 8), 
-    lsKey: 'elaina-blurConfigCardValue', 
-    style: '--theme-blur-config-card' 
-  }
-]);
-
-const insertCustomBlurStyle = () => {
-  const render = (name: string, value: number) => {
-    if (value != 0) return `${name}:blur(${value}px);`
-    else return '';
-  };
-
-  let style: string = ':root{';
-  for (let i = 0; i < elementLists.value.length; i++) {
-    style += render(elementLists.value[i].style, elementLists.value[i].value);
-  }
-  style += '};'
-
-  insertStyle('custom-blur', style);
-};
-
-const removeCustomBlurStyle = () => {
-  removeStyle('custom-blur');
-};
-
-const updateCustomBlur = (lsKey: string, value: number) => {
-  putLS(lsKey, value);
-  insertCustomBlurStyle();
-};
 
 const customBlurSwitch = (value: boolean) => {
   if (value == true) insertCustomBlurStyle();
